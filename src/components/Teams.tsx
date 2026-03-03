777248b04ac3ebd1090569646ce56a817cbb5e01
natiman8634 <natiman863400@gmail.com>
2026-03-02 20:15:02 -0800

    update

--- src/components/Teams.tsx
+++ src/components/Teams.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-import children from "../assets/children.jpg";
+import children from "../assets/20250610_182355.jpg";
 import funding from "../assets/funding.jpg";
 import bible from "../assets/bible.jpg";
 import treatment from "../assets/treatment.jpg";
-import prison from "../assets/prison.jpg";
+import prison from "../assets/hero2.jpg";
 
 
 const teams = [
